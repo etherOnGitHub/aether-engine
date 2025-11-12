58f9bf14f7ac0b4ff8beaabd7bc5b06c840c88f5
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-11-12 23:27:20 +0000

    add: init java framework + sdk setup

---
+++ src/main/java/com/aetherwave/engine/App.java
@@ -0,0 +1,7 @@
+package com.aetherwave.engine;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Aether Engine test build on Java " + System.getProperty("java.version"));
+    }
+}
